ee333ed8d5d61a92b713745547e5e07cf037f13f
Sonbeomgyu1 <sjk6285@naver.com>
2023-08-09 15:22:21 +0900

    0809(3시22분)
    .

---
+++ source/src/main/java/kh/semi/inter/contorller/mainServlet.java
@@ -0,0 +1,26 @@
+package kh.semi.inter.contorller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/inter1")
+public class mainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public mainServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
